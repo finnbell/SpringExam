1eef8c3157472f42246c44bad268f57ffb35a610
finnbell <hongcheongpyo@gmail.com>
2021-06-28 21:46:16 +0900

    Spring Boot with Mongo DB (Docker)

---
+++ src/test/java/com/cphong/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.cphong.springmongo;
+
+import com.cphong.springmongo.account.Account;
+import com.cphong.springmongo.account.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+
+
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
