8e195302886e8a1f344899aae325fade52a1af86
finnbell <finnbell@naver.com>
2022-03-01 23:15:34 +0900

    Embeded 값 타입 비교,   Address 클래스 내부에서 equals 를 자동으로 만든후 비교 하도록 한다.
    a == b true
    address1 == address2 : false
    address1 equals address2 : true

--- ex1hellojpa/src/main/java/hellojpa/ValueMain.java
+++ ex1hellojpa/src/main/java/hellojpa/ValueMain.java
@@ -5,12 +5,16 @@ import javax.persistence.criteria.CriteriaBuilder;
 public class ValueMain {
     public static void main(String[] args) {
 
-        Integer a = new Integer(10);
-        Integer b = a;
+        Integer a = 10;
+        Integer b = 10;
 
+        System.out.println("a == b " + (a == b));
 
-        System.out.println("a= " + a);
-        System.out.println("b= " + b);
+        Address address1 = new Address("city","street","10000");
+        Address address2 = new Address("city","street","10000");
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1 equals address2 : " + address1.equals(address2)) ;
+
 
     }
 }
