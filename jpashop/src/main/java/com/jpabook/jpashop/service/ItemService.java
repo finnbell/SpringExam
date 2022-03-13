eb830cd21487a61bded1ec4338a4df262e6cc992
finnbell <finnbell@naver.com>
2022-03-13 22:41:32 +0900

    주문 도메인 개발, Repository 까지..

--- jpashop/src/main/java/com/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/com/jpabook/jpashop/service/ItemService.java
@@ -1,31 +1,31 @@
 package com.jpabook.jpashop.service;
 
-import com.jpabook.jpashop.domain.item.Item;
-import com.jpabook.jpashop.repository.ItemRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.jpabook.jpashop.domain.item.item;
+import com.jpabook.jpashop.repository.itemrepository;
+import lombok.requiredargsconstructor;
+import org.springframework.stereotype.service;
+import org.springframework.transaction.annotation.transactional;
 
-import java.util.List;
+import java.util.list;
 
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class ItemService {
+@service
+@transactional(readonly = true)
+@requiredargsconstructor
+public class itemservice {
 
-    private final ItemRepository itemRepository;
+    private final itemrepository itemrepository;
 
-    @Transactional
-    public void saveItem(Item item) {
-        itemRepository.save(item) ;
+    @transactional
+    public void saveitem(item item) {
+        itemrepository.save(item) ;
     }
 
-    public List<Item> findItems() {
-        return itemRepository.findAll();
+    public list<item> finditems() {
+        return itemrepository.findall();
     }
 
-    public Item findOne(Long itemId)  {
-        return itemRepository.findOne(itemId);
+    public item findone(long itemid)  {
+        return itemrepository.findone(itemid);
     }
 
 }
