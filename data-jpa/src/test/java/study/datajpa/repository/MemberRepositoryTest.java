c40cb9c5cce7943cb3171e62aa57c10040039d16
finnbell <finnbell@naver.com>
2022-03-27 17:39:36 +0900

    스프링 DATA JPA  native sql 예제

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -28,7 +28,6 @@ class MemberRepositoryTest {
     @PersistenceContext
     EntityManager em;
 
-
     @Test
     void testMember() {
         Member member = new Member("memberA");
@@ -393,4 +392,69 @@ class MemberRepositoryTest {
 
         assertThat(result.get(0).getUsername()).isEqualTo("m1");
     }
+
+    @Test
+    void projections() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member ("m1", 0, teamA);
+        Member m2 = new Member ("m2", 0, teamA);
+
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+//        List<UsernameOnlyDto> result  = memberRepository.findProjectionsByUsername("m1", UsernameOnlyDto.class);
+
+//        for (NestedClosedProjections usernameOnly : result) {
+//            System.out.println(("usernameOnly = "+ usernameOnly));
+//        }
+
+        List<NestedClosedProjections> result  = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class );
+
+        for (NestedClosedProjections nestedClosedProjections : result) {
+            String username = nestedClosedProjections.getUsername();
+            System.out.println("username = "+ username);
+
+            String teamName = nestedClosedProjections.getTeam().getName();
+            System.out.println("teamName = " + teamName);
+        }
+
+    }
+
+
+    @Test
+    void nativeQuery() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member ("m1", 0, teamA);
+        Member m2 = new Member ("m2", 0, teamA);
+
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+//        Member result = memberRepository.findByNativeQuery("m1");
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection = "+ memberProjection.getUsername());
+            System.out.println("memberProjection = "+ memberProjection.getTeamName());
+
+        }
+
+
+    }
+
+
 }
