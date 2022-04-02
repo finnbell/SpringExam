9134791b2f333966fe00e4924ead3d1afddb2acc
finnbell <finnbell@naver.com>
2022-04-02 23:14:40 +0900

    querydsl fetch join 사용 예제, N+1 문제 해결 및 성능 향상에 필수!

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -12,6 +12,9 @@ import study.querydsl.entity.Member;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
@@ -325,6 +328,44 @@ public class QuerydslBasicTest {
 
 
 
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+
+    @Test
+    void fetchJoinNo() throws Exception {
+
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+
+    }
+
+    @Test
+    void fetchJoinUse() throws Exception {
+
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+
+    }
 
 
 
