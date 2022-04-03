621fa1bfe5cf91f3f167f3addd0d9a6b8a153a64
finnbell <finnbell@naver.com>
2022-04-03 23:23:55 +0900

    querydsl Tuple , Tuple Projection 예제

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -556,5 +556,35 @@ public class QuerydslBasicTest {
 
 
 
+    @Test
+    void simpleProjection() throws Exception {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = "+ s);
+        }
+
+    }
+
+    @Test
+    public void tupleProjection() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = "+ username);
+            System.out.println("age = "+ age);
+        }
+    }
+
+
 
 }
