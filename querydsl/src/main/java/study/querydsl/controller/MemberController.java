76478d7e6cf23e4bd88452ddc472d50a7a62b409
finnbell <finnbell@naver.com>
2022-04-18 23:04:06 +0900

    querydsl  spring DATA JPA 페이징 활용및 querydslPredicateExecutor (잘 안쓰임, join 지원 못함)

--- querydsl/src/main/java/study/querydsl/controller/MemberController.java
+++ querydsl/src/main/java/study/querydsl/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
@@ -15,9 +17,25 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
     }
+
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberv3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition,pageable);
+    }
+
+
+
+
 }
