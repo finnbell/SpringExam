3b9bf6ce13e4bea7bf50298e885f64a792e4fb97
finnbell <finnbell@naver.com>
2022-04-28 22:29:57 +0900

    Entity 설계

---
+++ jpashop3/src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello JPA";
+    }
+
+}
