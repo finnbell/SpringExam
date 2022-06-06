b1e5038c888f2cf840082a246c91c36f9b9c855f
finnbell <finnbell@naver.com>
2022-06-06 16:45:21 +0900

    Spring 컨테이너에서 Bean 조회 하기

--- springcore/src/main/java/springcore/hello/AppConfig.java
+++ springcore/src/main/java/springcore/hello/AppConfig.java
@@ -1,7 +1,10 @@
 package springcore.hello;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import springcore.hello.discount.DisocuntPolicy;
 import springcore.hello.discount.FixDiscountPolicy;
+import springcore.hello.discount.RateDiscountPolicy;
 import springcore.hello.member.MemberRepository;
 import springcore.hello.member.MemberService;
 import springcore.hello.member.MemberServiceImpl;
@@ -9,23 +12,28 @@ import springcore.hello.member.MemoryMemberRepository;
 import springcore.hello.order.OrderService;
 import springcore.hello.order.OrderServiceImpl;
 
+@Configurable
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), disocuntPolicy());
     }
 
+    @Bean
     private DisocuntPolicy disocuntPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
