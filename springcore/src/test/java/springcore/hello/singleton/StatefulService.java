09086e85d92953bc73844e5c5a4085a2c3986f7a
finnbell <finnbell@naver.com>
2022-06-07 23:56:18 +0900

    singleton 패턴 예제,  singletone 오류 발생 예제

---
+++ springcore/src/test/java/springcore/hello/singleton/StatefulService.java
@@ -0,0 +1,18 @@
+package springcore.hello.singleton;
+
+public class StatefulService {
+
+    private int price;
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        this.price = price;
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+
+}
