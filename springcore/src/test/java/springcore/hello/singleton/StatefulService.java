c66072ba5cba2abd48fe11d19aca23a861763db6
finnbell <finnbell@naver.com>
2022-06-07 23:59:42 +0900

    singletone 오류 발생 해결방법은 멤버 변수를 사용하지 않고, 바로 리턴하도록 함.

--- springcore/src/test/java/springcore/hello/singleton/StatefulService.java
+++ springcore/src/test/java/springcore/hello/singleton/StatefulService.java
@@ -2,17 +2,20 @@ package springcore.hello.singleton;
 
 public class StatefulService {
 
-    private int price;
+//    private int price;
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price;
-    }
+//        this.price = price;
 
-
-    public int getPrice() {
         return price;
     }
+
+
+
+//    public int getPrice() {
+////        return price;
+//    }
 
 
 }
