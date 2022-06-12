6883a46b8e899ab2583d287b7b6b6229e613da7a
finnbell <finnbell@naver.com>
2022-06-12 11:58:13 +0900

    SingleTon 클래스 호출 파악하기

--- springcore/src/main/java/springcore/hello/AppConfig.java
+++ springcore/src/main/java/springcore/hello/AppConfig.java
@@ -1,7 +1,7 @@
 package springcore.hello;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springcore.hello.discount.DiscountPolicy;
 import springcore.hello.discount.RateDiscountPolicy;
 import springcore.hello.member.MemberRepository;
@@ -11,26 +11,33 @@ import springcore.hello.member.MemoryMemberRepository;
 import springcore.hello.order.OrderService;
 import springcore.hello.order.OrderServiceImpl;
 
-@Configurable
+@Configuration
 public class AppConfig {
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    private MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), disocuntPolicy());
     }
 
     @Bean
-    private DiscountPolicy disocuntPolicy() {
+    public DiscountPolicy disocuntPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
