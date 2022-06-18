97ae6a051a9172de9ce3681ace1bf6c8a9010336
finnbell <finnbell@naver.com>
2022-06-18 15:46:22 +0900

    Bean 초기화, @PostConstruct, @PreDestroy  호출

--- springcore/src/test/java/springcore/hello/lifecycle/NetworkClient.java
+++ springcore/src/test/java/springcore/hello/lifecycle/NetworkClient.java
@@ -1,7 +1,7 @@
 package springcore.hello.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class NetworkClient {
 
@@ -32,16 +32,15 @@ public class NetworkClient {
     }
 
 
-    @Override
+
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
-
-
     }
 
-    @Override
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
