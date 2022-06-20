0ac49f2b228ee4c33d2d1466e4cd8cd380b872e0
finnbell <finnbell@naver.com>
2022-06-21 07:15:14 +0900

    componentScan 의  IncludeFilter, ExcludeFilter 활용법

---
+++ springcore/src/test/java/springcore/hello/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package springcore.hello.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ springcore/src/test/java/springcore/hello/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package springcore.hello.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
