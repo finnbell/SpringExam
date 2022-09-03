43e7feec46da5b06c4c07cdb6121d895201d44fc
finnbell <finnbell@naver.com>
2022-09-03 22:07:08 +0900

    Rest API  Exception 사용자 에러 처리방법

---
+++ SpringBootRestfulWebService/restful-web-service/src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.example.restfulwebservice.exception;
+
+import com.example.restfulwebservice.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), "ServerError", ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request)  {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), "Not Found",ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+}
