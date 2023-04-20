3380297f1b3afe56b5c6fa47073ab7c60d80ac0c
finn <finnbell@naver.com>
2023-04-20 23:22:29 +0900

    http://localhost:8088/v3/api-docs, http://localhost:8088/swagger-ui/ swagger 적용 schema 항목에 추가됨을 확인함

--- SpringBootRestfulWebService/restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ SpringBootRestfulWebService/restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,17 +21,23 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value={"password","ssn"})
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes="사용자 이름을 입력해 주세요")
     private String name;
 
     @Past
+    @ApiModelProperty(notes="사용자 등록일을 입력해 주세요")
     private Date joinDate;
 
+    @ApiModelProperty(notes="사용자 패스워드를 입력해 주세요")
     private String password;
+
+    @ApiModelProperty(notes="사용자 주민번호를 입력해 주세요")
     private String ssn;
 
 }
