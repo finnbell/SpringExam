4a500c1e16155d12cb7b3b639f18ce05b1bc9256
finn <finnbell@naver.com>
2023-04-26 23:31:56 +0900

    spring security 적용하고 h2 db 1.493 버전( 상위 버전부터 접속방식 변경됨) 연결시 브라우저에서 http://localhost:8088/h2-console 접속 jdbc url:jdbc:h2:mem:testdb username : sa Connect 연결

--- SpringBootRestfulWebService/restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ SpringBootRestfulWebService/restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -1,14 +1,38 @@
 package com.example.restfulwebservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+
+//                http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/", "/home").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin((form) -> form
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+//                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
