3ea3931f60d934e7d2dc31e08cf025aabfb7257f
finn <finnbell@naver.com>
2023-05-20 15:41:07 +0900

    주석 처리 방법 3가지

--- mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -124,6 +124,14 @@ public class BasicController {
         return "basic/condition";
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+
+
     @Data
     static class User {
         private String username;
