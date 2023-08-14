8f03d32fbd294b8f159b61d8486a64bf865d4d85
finn <finnbell@naver.com>
2023-08-14 21:59:48 +0900

    login 프로젝트 - 인터셉터로 필터와 동일기능 구현. 앞으로 인터셉터를 쓰자!!

--- mvc2/login/src/main/java/hello/login/web/WebConfig.java
+++ mvc2/login/src/main/java/hello/login/web/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login.web;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
-import javax.servlet.FilterRegistration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,6 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
     }
 
 
