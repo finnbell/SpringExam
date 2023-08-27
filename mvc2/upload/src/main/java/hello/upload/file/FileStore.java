eda2d79fa0b8b5e7387376d71bce6b249556bd7a
finn <finnbell@naver.com>
2023-08-28 00:16:16 +0900

    파일 업로드 - 첨부파일, 파일 다중 선택및 조회 기능(이미지 뷰어) 구현

---
+++ mvc2/upload/src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,64 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.attoparser.trace.MarkupTraceEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if(multipartFile.isEmpty()) {
+            return null;
+        }
+
+        //원래 파일명
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        // 서버에 저장하는 파일명;
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if(!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+
+        return storeFileResult;
+    }
+
+    private String createStoreFileName(String origfilename) {
+        String ext = extractExt(origfilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+
+
+}
